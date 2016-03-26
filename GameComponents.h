a0a0a0f145c0b54051a817f2898a57ec24880aec
Rodolfo377 <rodolfo.fbf@gmail.com>
2016-03-26 14:20:09 -0300

    Images are loaded on a separate file

--- GameComponents.h
+++ GameComponents.h
@@ -8,82 +8,104 @@
 
 class Elements
 {
-	
+	/*The order of the elements, in each of the arrays below is: Rose, Violet, Sunflower
+	which are corresponding to their respective type of flower */
 public:
-
-	/*
-	//Variables
-	int beeXVelocity = 0;
-	int beeYVelocity = 0;
-
-	//States
-	bool play = true;
-	bool gif_fly = true;
-	bool up = false;
-	bool down = false;
-	bool left = false;
-	bool right = false;
-	bool fly_left = false;
-	bool fly_right = true;
-	//Images
+	sf::Texture flower_tex[3];
 	sf::Texture bee1_right;
 	sf::Texture bee2_right;
 	sf::Texture bee1_left;
 	sf::Texture bee2_left;
 	sf::Texture painting;
 
-	if (bee1_right.loadFromFile("Images/bee_worker_flying_1_right.png") == false)
-	{
-		return 1;
-	}
-	if (bee2_right.loadFromFile("Images/bee_worker_flying_2_right.png") == false)
-	{
-		return 1;
-	}
-
-	if (bee1_left.loadFromFile("Images/bee_worker_flying_1_left.png") == false)
-	{
-		return 1;
-	}
-	if (bee2_left.loadFromFile("Images/bee_worker_flying_2_left.png") == false)
-	{
-		return 1;
-	}
-
-	if (painting.loadFromFile("Images/Scenario/Background/landscape under stormy sky .png") == false)
-	{
-		return 1;
-	}
+	const int WIDTH = 1000;
+	const int HEIGHT = 600;
 
 
-	//Shapes
 	sf::RectangleShape background;
-	background.setSize(sf::Vector2f(WIDTH, HEIGHT));
-	background.setPosition(0, 0);
-	background.setTexture(&painting);
-
+	sf::RectangleShape flowers[3];
 	sf::RectangleShape player1;
-	player1.setSize(sf::Vector2f(30, 30));
-	player1.setPosition(800, 200);
-	player1.setTexture(&bee1_right);
-	//Sound
+	
 
-	//Music
+public:
 
-
-
-	//takes two textures as arguments and alternates between them during a given period, in ms. Returns 
-	//one of the two images each time it is called, based on the timing.
-	sf::Texture animate(sf::Texture img1, sf::Texture img2, int milliseconds)
+	Elements()
 	{
+		if (!flower_tex[0].loadFromFile("Images/Scenario/flowers/rose_full.png"))
+		{
+			std::cout << "Could not find 'rose_full.png' image...\n";
+		}
 
-	}*/
+		if (!flower_tex[1].loadFromFile("Images/Scenario/flowers/violet_full.png"))
+		{
+			std::cout << "Could not find 'violet_full.png' image...\n";
+		}
 
-	int x = 5;
+		if (!flower_tex[2].loadFromFile("Images/Scenario/flowers/sunflower_full.png"))
+		{
+			std::cout << "Could not find 'sunflower_full.png' image...\n";
+		}
 
+		if (bee1_right.loadFromFile("Images/bee_worker_flying_1_right.png") == false)
+		{
+			std::cout << "Could not find 'bee_worker_flying_1_right.png' image...\n";
+		}
+		if (bee2_right.loadFromFile("Images/bee_worker_flying_2_right.png") == false)
+		{
+			std::cout << "Could not find 'bee_worker_flying_2_right.png' image...\n";
+		}
 
-	void print()
+		if (bee1_left.loadFromFile("Images/bee_worker_flying_1_left.png") == false)
+		{
+			std::cout << "Could not find 'bee_worker_flying_1_left.png' image...\n";
+		}
+		if (bee2_left.loadFromFile("Images/bee_worker_flying_2_left.png") == false)
+		{
+			std::cout << "Could not find 'bee_worker_flying_2_left.png' image...\n";
+		}
+
+		if (painting.loadFromFile("Images/Scenario/Background/landscape under stormy sky .png") == false)
+		{
+			std::cout << "Could not find 'landscape under stormy sky .png' image...\n";
+		}
+	}
+
+	//This loads the image on the window, placing each of the RectangleShape objects in a certain place
+	void load_flowers()
 	{
-		std::cout << "Header function print has been called!\n";
+		for (int i = 0; i < 3; i++)
+		{
+			flowers[i].setSize(sf::Vector2f(100, 100));
+			flowers[i].setPosition((i +1)* 200, 500);
+			flowers[i].setTexture(&flower_tex[i]);
+		}
+	}
+
+	sf::RectangleShape getFlower(int index)
+	{
+		return flowers[index];
+	}
+
+	void load_background()
+	{
+		background.setSize(sf::Vector2f(WIDTH, HEIGHT));
+		background.setPosition(0, 0);
+		background.setTexture(&painting);
+	}
+
+	sf::RectangleShape getBackground()
+	{
+		return background;
+	}
+
+	void load_player1()
+	{
+		player1.setSize(sf::Vector2f(30, 30));
+		player1.setPosition(800, 200);
+		player1.setTexture(&bee1_right);
+	}
+	sf::RectangleShape getPlayer1()
+	{
+		return player1;
 	}
 };
--- Main.cpp
+++ Main.cpp
@@ -16,27 +16,28 @@
 #include "GameComponents.h"
 
 
-const int WIDTH = 1000;
-const int HEIGHT = 600;
-sf::Event event;
+
+
 
 int main()
 {
+
+	const int WIDTH = 1000;
+	const int HEIGHT = 600;
 	sf::Event event;
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "BeeHero");
 	window.setFramerateLimit(60);
 	window.setKeyRepeatEnabled(false);
 	
 	Elements el;
-
-	el.print();
-	std::cout << "x is "<<el.x << "\n";
+	//KeyboardEvents ke;
 	
 	//Variables
 	int beeXVelocity = 0;
 	int beeYVelocity = 0;
 
 	//States
+	
 	bool play = true;
 	bool gif_fly = true;
 	bool up = false;
@@ -45,51 +46,18 @@ int main()
 	bool right = false;
 	bool fly_left = false;
 	bool fly_right = true;
+	
 	//Images
-	sf::Texture bee1_right;
-	sf::Texture bee2_right;
-	sf::Texture bee1_left;
-	sf::Texture bee2_left;
-	sf::Texture painting;
-
-	if (bee1_right.loadFromFile("Images/bee_worker_flying_1_right.png") == false)
-	{
-		return 1;
-	}
-	if (bee2_right.loadFromFile("Images/bee_worker_flying_2_right.png") == false)
-	{
-		return 1;
-	}
-
-	if (bee1_left.loadFromFile("Images/bee_worker_flying_1_left.png") == false)
-	{
-		return 1;
-	}
-	if (bee2_left.loadFromFile("Images/bee_worker_flying_2_left.png") == false)
-	{
-		return 1;
-	}
-
-	if (painting.loadFromFile("Images/Scenario/Background/landscape under stormy sky .png") == false)
-	{
-		return 1;
-	}
 	
 
-	//Shapes
-	sf::RectangleShape background;
-	background.setSize(sf::Vector2f(WIDTH, HEIGHT));
-	background.setPosition(0, 0);
-	background.setTexture(&painting);
+	el.load_flowers();
+	el.load_background();
+	el.load_player1();
 
-	sf::RectangleShape player1;
-	player1.setSize(sf::Vector2f(30, 30));
-	player1.setPosition(800, 200);
-	player1.setTexture(&bee1_right);
 	//Sound
 
 	//Music
-
+	
 	while (play == true)
 	{
 		
@@ -100,6 +68,11 @@ int main()
 			if (event.type == sf::Event::Closed)
 				play = false;
 
+			/*ke.getEvents(event, el);
+			ke.runLogic();
+			ke.flapWings();*/
+			/*KeyBoardEvents*/
+			
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
 				play = false;
 
@@ -112,7 +85,8 @@ int main()
 				fly_right = false;
 			}
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S){
-				down = true; player1.setTexture(&bee1_right);
+				down = true; 
+				el.player1.setTexture(&el.bee1_right);
 			}
 			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D){
 				fly_left = false;
@@ -132,7 +106,6 @@ int main()
 
 		/****************Logic******************/
 
-
 		if (up == true) { beeYVelocity = -2; }
 		if (left == true) { beeXVelocity = -2;}
 		if (down == true) {beeYVelocity = 2;}
@@ -140,7 +113,7 @@ int main()
 		if (down == false && up == false) { beeYVelocity = 0; }
 		if (left == false && right == false) { beeXVelocity = 0; }
 		
-		player1.move(beeXVelocity, beeYVelocity);
+		el.player1.move(beeXVelocity, beeYVelocity);
 		
 		//Makes the bee flap its wings
 
@@ -148,11 +121,11 @@ int main()
 		{
 			if (fly_left == true)
 			{
-				player1.setTexture(&bee1_left);
+				el.player1.setTexture(&el.bee1_left);
 			}
 			if (fly_right == true)
 			{
-				player1.setTexture(&bee1_right);
+				el.player1.setTexture(&el.bee1_right);
 			}
 			gif_fly = true;
 		}
@@ -161,11 +134,11 @@ int main()
 		{
 			if (fly_left == true)
 			{
-				player1.setTexture(&bee2_left);
+				el.player1.setTexture(&el.bee2_left);
 			}
 			if (fly_right == true)
 			{
-				player1.setTexture(&bee2_right);
+				el.player1.setTexture(&el.bee2_right);
 			}
 			
 			gif_fly = false;
@@ -173,8 +146,15 @@ int main()
 		/****************Rendering******************/
 
 		window.clear();
-		window.draw(background);
-		window.draw(player1);
+		window.draw(el.background);
+
+		for (int j = 0; j < 3; j++)
+		{
+			window.draw(el.getFlower(j));
+		}
+
+		window.draw(el.player1);
+		
 		window.display();
 	}
 
