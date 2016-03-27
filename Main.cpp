378efb8acd5a1fd72520caa59a912bf24dc6dd1d
Rodolfo377 <rodolfo.fbf@gmail.com>
2016-03-27 18:29:07 -0300

    Bee lands on flowers, Main does all the job

--- Main.cpp
+++ Main.cpp
@@ -15,13 +15,37 @@
 #include <SFML/Graphics/Image.hpp>
 #include "GameComponents.h"
 
+/*
+class Variables
+{
+public:
+	Elements el;
 
+	bool collecting = false;
+	bool stop = false;
+	bool leave = false;
 
+	void flower_in_smooth(int, int);
+	void flower_out_smooth(int, int);
+	void flower_stop();
+};
 
+*/
+
+//void flower_in_smooth(int, int);
+/*void flower_out_smooth(int, int);
+void flower_stop();*/
+
+Elements el;
+
+bool collecting = false;
+bool stop = false;
+bool leave = false;
 
 int main()
 {
 
+	//Variables v;
 	const int WIDTH = 1000;
 	const int HEIGHT = 600;
 	sf::Event event;
@@ -30,11 +54,21 @@ int main()
 	window.setKeyRepeatEnabled(false);
 	
 	Elements el;
+
+	bool collecting = false;
+	bool stop = false;
+	bool leave = false;
+
+	
+	
 	//KeyboardEvents ke;
 	
 	//Variables
 	int beeXVelocity = 0;
 	int beeYVelocity = 0;
+	int tempX = 0;
+	int tempY = 0;
+	int flower_id = 0;
 
 	//States
 	
@@ -94,6 +128,17 @@ int main()
 				right = true;
 			}
 
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Z)
+			{
+				//collecting = false;
+				std::cout << "Z\n";
+				stop = false;
+				leave = true;
+				//el.player1.setPosition(tempX + 20, tempY - 100);
+				//tempX = 0;
+				//tempY = 0;
+			}
+
 			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::W)
 				up = false;
 			if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::A)
@@ -143,6 +188,71 @@ int main()
 			
 			gif_fly = false;
 		}
+
+		//Scanning
+		for (int i = 0; i < 3; i++)
+		{
+			if (el.player1.getGlobalBounds().intersects(el.flowers[i].getGlobalBounds()) == true && leave == false)
+			{
+				collecting = true;
+				flower_id = i;
+			}
+		}
+
+		if (collecting == true)
+		{
+			 tempX = el.flowers[flower_id].getPosition().x;
+			 tempY = el.flowers[flower_id].getPosition().y;
+
+			//el.player1.move(tempX, tempY);
+			
+			 if (stop == false && leave == false)
+			 {
+				 
+				 //Smooth placement onto the flower
+				 if (el.player1.getPosition().x < tempX + 20)
+					 el.player1.move(1, 0);
+				 if (el.player1.getPosition().y < tempY)
+					 el.player1.move(0, 1);
+				 if (el.player1.getPosition().x > tempX + 20)
+					 el.player1.move(-1, 0);
+				 if (el.player1.getPosition().y > tempY)
+					 el.player1.move(0, -1);
+				 if (el.player1.getPosition().x == tempX + 20 && el.player1.getPosition().y == tempY)
+					 stop = true;
+				 //flower_in_smooth(tempX, tempY);
+			 }
+			 //Holds the bee on the flower while it collects pollen
+			 if (stop == true)
+			 {
+				 std::cout << "Stop == true\n";
+				 fly_right = true;
+				 el.player1.setPosition(tempX + 20, tempY);
+			 }
+
+			 //Smooth transition out of the flower
+			 if (stop == false && leave == true)
+			 {
+				 if (el.player1.getPosition().y >= tempY - 40)
+				 {
+					 el.player1.move(0, -1);
+					 std::cout << " leaving...\n";
+				 }
+				 else
+				 { 
+					 std::cout << " left!\n";
+					 leave = false;
+					 collecting = false;
+					 /*
+						resets flower_index to a non-existing array index, 
+						and it will only be valid once it is redifined on 
+						the next Scanning that finds a collision with one 
+						of the flowers and the bee.*/
+					 flower_id = 1000;
+				 }
+			 }
+			 
+		}
 		/****************Rendering******************/
 
 		window.clear();
@@ -156,8 +266,39 @@ int main()
 		window.draw(el.player1);
 		
 		window.display();
+
+		//std::cout << "\ncollecting: " << collecting << " tempX = " << tempX << " tempY = " << tempY;
 	}
 
 	window.close();
 	return 0;
 }
+
+/*void flower_in_smooth(int tempX, int tempY)
+
+	
+	if (el.player1.getPosition().x < tempX + 20)
+		el.player1.move(1, 0);
+	if (el.player1.getPosition().y < tempY)
+		el.player1.move(0, 1);
+	if (el.player1.getPosition().x > tempX + 20)
+	{
+		el.player1.move(-1, 0);
+		std::cout << "\n\n moving!\n\n";
+	}
+	if (el.player1.getPosition().y > tempY)
+		el.player1.move(0, -1);
+	if (el.player1.getPosition().x == tempX + 20 && el.player1.getPosition().y == tempY)
+		stop = true;
+}
+/*
+void flower_stop()
+{
+
+}
+
+void flower_out_smooth(int tempX, int tempY)
+{
+
+}
+*/
